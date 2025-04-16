673c412744445cb9443d57a39a2103b225ca7658
Nuria <nuriaa@ethz.ch>
2025-04-16 14:43:55 +0200

    adding eval configs and correl plot

--- plot/correl.py
+++ plot/correl.py
@@ -8,24 +8,35 @@ from sklearn.linear_model import LinearRegression
 # x = np.random.rand(100, 1) * 10  # Random values between 0 and 10
 # y = 2.5 * x + np.random.randn(100, 1) * 3  # Linear relation with noise
 
-name_exp = 'goals_18_det'
-y = np.loadtxt(f"/home/nuria/phd/controllable_agent/figs/correl/ef_{name_exp}.csv", delimiter=",").reshape(-1,1)
-x = np.loadtxt(f"/home/nuria/phd/controllable_agent/figs/correl/eq_{name_exp}.csv", delimiter=",").reshape(-1,1)
+name_exp = 'goals_18'
+folder = 'maze'
+y = np.loadtxt(f"/home/nuria/phd/controllable_agent/figs/correl/maze/ef_{name_exp}.csv", delimiter=",").reshape(-1,1)
+x = np.loadtxt(f"/home/nuria/phd/controllable_agent/figs/correl/maze/eq_{name_exp}.csv", delimiter=",").reshape(-1,1)
 
+# name_exp = 'tr_mean'
+# folder = 'walker'
+# y = np.loadtxt(f"/home/nuria/phd/controllable_agent/figs/correl/walker/ef_{name_exp}.csv", delimiter=",").reshape(-1,1)
+# x = np.loadtxt(f"/home/nuria/phd/controllable_agent/figs/correl/walker/eq_{name_exp}.csv", delimiter=",").reshape(-1,1)
+
+# x_outl = x[x<400]
+# y_outl = y[x<400]
+
+# x = x_outl[y_outl<40000]
+# y = y_outl[y_outl<40000]
+
+# STANDARDIZE
+x = (x - np.mean(x))/np.std(x)
+y = (y - np.mean(y))/np.std(y)
+
+if len(x.shape) < 2:
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
 
 plt.rcParams.update(bundles.iclr2024(
     family="serif", rel_width=0.9, nrows=1.0, ncols=1.0, usetex=False))
 
 ourblue = (0.368, 0.507, 0.71)
 ourdarkblue = (0.368, 0.607, 0.9)
-ourorange = (0.881, 0.611, 0.142)
-ourgreen = (0.56, 0.692, 0.195)
-ourred = (0.923, 0.386, 0.209)
-ourviolet = (0.528, 0.471, 0.701)
-ourbrown = (0.772, 0.432, 0.102)
-ourlightblue = (0.364, 0.619, 0.782)
-ourdarkgreen = (0.572, 0.586, 0.0)
-ourdarkred = (0.923, 0.386, 0.)
 
 # Fit a linear regression model
 model = LinearRegression()
@@ -39,6 +50,6 @@ plt.xlabel("Var[Q]")
 # plt.ylabel("Det(Covar[F])")
 plt.ylabel("tr(Covar[F])")
 # plt.show()
-dir_figs = '/home/nuria/phd/controllable_agent/figs/correl'
-name_fig = f'correl_{name_exp}'
+dir_figs = f'/home/nuria/phd/controllable_agent/figs/correl/{folder}'
+name_fig = f'correl_{name_exp}_standardized'
 plt.savefig(f'{dir_figs}/{name_fig}.pdf', bbox_inches='tight')
